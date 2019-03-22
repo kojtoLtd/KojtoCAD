8929f1611cc4c7dd7da5c78a425add1eb7938bf4
AngelKostadinov <kostadinov_angel@hotmail.com>
2019-03-22 09:41:11 +0200

    Settings fix

--- KojtoCAD.2013/Properties/Settings.Designer.cs
+++ KojtoCAD.2013/Properties/Settings.Designer.cs
@@ -1729,7 +1729,7 @@ namespace KojtoCAD.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
         public bool WebTrackerIsEnabled {
             get {
                 return ((bool)(this["WebTrackerIsEnabled"]));
